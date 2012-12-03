7416281e05da08bd240c9343fc63f4bf25e329c3
Chad Stümpges <chad.stuempges@wooga.net>
2012-12-03 21:46:51 +0100

    updated to newest percentjuice/grapefrukt library.

---
+++ example/F2C_RobotTutorial/AppLogicTests/AppLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  AppLogicTests.h
+//  AppLogicTests
+//
+//  Created by Chad Stuempges on 12/3/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppLogicTests : SenTestCase
+
+@end
---
+++ example/F2C_RobotTutorial/AppLogicTests/AppLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  AppLogicTests.m
+//  AppLogicTests
+//
+//  Created by Chad Stuempges on 12/3/12.
+//
+//
+
+#import "AppLogicTests.h"
+
+@implementation AppLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AppLogicTests");
+}
+
+@end
